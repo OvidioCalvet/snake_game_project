7977d50dc3f6f42273b05e9738add25c62f0bcf5
Ovidio Calvet <calvetovidio@gmail.com>
2024-11-22 07:46:12 -0500

    new: creates a snake class with attribute segments and initializes the screen where the game is ran.

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+from snake import Snake
+
+screen = Screen()
+
+screen.setup(width=600.0, height=600.0)
+
+screen.bgcolor('black')
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+
+class Snake:
+
+    def __init__(self, segments):
+        
+        self.segments = segments
