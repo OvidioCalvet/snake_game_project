9f9685de798f62aec30134d1d74b667e8460dd0c
Ovidio Calvet <ovidiocalvet@Ovidios-MacBook-Pro.local>
2024-11-22 10:34:53 -0500

    wip: adds more additions to the snake class and initalizes a snake in the main file printing on the turtle screen

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ screen.setup(width=600.0, height=600.0)
 
 screen.bgcolor('black')
 
+snake = Snake()
+snake.create_snake()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,7 +1,19 @@
 from turtle import Turtle
 
+STARTING_POSITIONS: list = [(0,0), (-20,0), (-40,0)]
+
 class Snake:
 
-    def __init__(self, segments):
+    def __init__(self):
         
-        self.segments = segments
+        self.segments = []
+    
+    def create_snake():
+
+        for position in STARTING_POSITIONS:
+
+            new_segment = Turtle()
+
+            new_segment.shape('square')
+            new_segment.color('white')
+            new_segment.position(STARTING_POSITIONS[position])
