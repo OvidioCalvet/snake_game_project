d79ee05029f6a519ba41cf89674011fe107c6e61
Ovidio Calvet <ovidiocalvet@Ovidios-MacBook-Pro.local>
2024-12-02 11:19:02 -0500

    wip: sets up next step to complete the game by adding a scoreboard class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
---
+++ scoreboard.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+
+class Scoreboard:
+
+    def __init__(self):
+        
+        pass
